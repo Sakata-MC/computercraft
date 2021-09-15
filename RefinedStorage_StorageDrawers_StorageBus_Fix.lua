3580115daa23f07ff3c8d207e12d2e2f82152176
Sakata-MC <liath.ww@gmail.com>
2021-09-14 23:18:04 -0400

    Update RefinedStorage_StorageDrawers_StorageBus_Fix.lua

--- RefinedStorage_StorageDrawers_StorageBus_Fix.lua
+++ RefinedStorage_StorageDrawers_StorageBus_Fix.lua
@@ -1,1 +1,20 @@
+-- This should get the stupid Refined Storage
+-- storage bus to properly read the Storage
+-- Drawers Controller.
+-- Basically any time the chunk is loaded this
+-- computer should run 'startup' and then turn
+-- off the "External Storage" RS cable to the
+-- right of this computer (it is set to de-
+-- activate on redstone signal)
+-- "front", "back", "left", "right", "top" or
+-- "bottom"
 
+varSide=left
+
+print("Emitting redstone to "..varSide.." side for 5s...")
+redstone.setOutput(varSide, true)
+os.sleep(5)
+print("Turning off redstone signal. Cable should")
+print("  now be on...")
+redstone.setOutput(varSide, false)
+return 0
